a993ca08bc5d5e8793cd7e3121c511b3b560d1d1
clara wilner <clara.luna.wilner@gmail.com>
2023-12-15 10:00:46 +0200

    essai 2

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -10,4 +10,18 @@ class RestaurantsController < ApplicationController
   def new
     @restaurant = Restaurant.new
   end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    @restaurant.save
+    raise
+  # No need for app/views/restaurants/create.html.erb
+    redirect_to restaurant_path(@restaurant)
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :rating)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   get "restaurants", to: "restaurants#index"
-  get "restaurants/:id", to: "restaurants#show"
   get "restaurants/new", to: "restaurants#new"
+  get "restaurants/:id", to: "restaurants#show"
+  post "restaurants", to: "restaurants#create"
 end
+
+
